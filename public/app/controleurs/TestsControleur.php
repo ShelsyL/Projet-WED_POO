6b114e676afec3593efd10a14b4d62d2cd0f9b71
Chelsea <chelsealouis.graphiste@gmail.com>
2020-12-14 10:11:23 +0100

    Etape TestControleur OK

---
+++ public/app/controleurs/TestsControleur.php
@@ -0,0 +1,12 @@
+<?php
+
+// On crée un espace de nom
+namespace App\Controleurs;
+
+// On définit une classe TestControleur
+class TestsControleur {
+
+  public function __construct(){
+    echo "Coucou TestControleur";
+   }
+ }
--- public/www/index.php
+++ public/www/index.php
@@ -6,12 +6,14 @@
 // Le role du dispatcher est de charger ...
   // 1. INIT - On charge l'initialisation
   // Le but de init c'est de définir les zones dynamiques et de se connecter à la base de données
-	require_once '../noyau/init.php';
+		// require_once '../noyau/init.php';
+		require_once '../vendor/autoload.php';
+		$monTest = new App\Controleurs\TestsControleur();
 
   // 2. ROUTEUR - On charge le routeur
   // Le role du routeur c'est d'hydrater les zones dynamiques
-	require_once '../app/routeur.php';
+		// require_once '../app/routeur.php';
 
   // 3. TEMPLATE - On charge le template
   // Le Role du template c'est d'afficher les zones dynamiques
-	require_once '../app/vues/template/index.php'; // On charge le template
+		// require_once '../app/vues/template/index.php'; // On charge le template
